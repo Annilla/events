dfb5cc2b645f81109a967653004282c36cd507f3
Anny <anny@juksy.com>
2017-02-20 18:19:02 +0800

    新增模板 video02

--- js/index.js
+++ js/index.js
@@ -794,7 +794,10 @@
     var player,
       header_02_player = $('#header_02_player'),
       header_02_videoId = header_02_player.data('videoid'),
-      header_02_start = header_02_player.data('start');
+      header_02_start = header_02_player.data('start'),
+      video_02_player = $('#video_02_player'),
+      video_02_videoId = video_02_player.data('videoid'),
+      video_02_start = video_02_player.data('start');
     // whenever youtube callback was called = deferred resolved
     // your custom function will be executed with YT as an argument
     YTdeferred.done(function(YT) {
@@ -821,6 +824,39 @@
         events: {
           onReady: function(e) {
             e.target.mute(); // 靜音
+          }
+        }
+      });
+
+      player = new YT.Player('video_02_player', {
+        videoId: video_02_videoId, // YouTube 影片ID
+        width: 1280, // 播放器寬度 (px)
+        height: 720, // 播放器高度 (px)
+        playerVars: {
+          rel: 0, // 播放結束後推薦其他影片
+          controls: 0, // 在播放器顯示暫停／播放按鈕
+          start: video_02_start, //指定起始播放秒數
+          autoplay: 0, // 在讀取時自動播放影片
+          loop: 1, // 讓影片循環播放
+          playlist: video_02_videoId,
+          showinfo: 0, // 隱藏影片標題
+          modestbranding: 1, // 隱藏YouTube Logo
+          fs: 0, // 隱藏全螢幕按鈕
+          cc_load_policty: 0, // 隱藏字幕
+          iv_load_policy: 3, // 隱藏影片註解
+          autohide: 0 // 當播放影片時隱藏影片控制列
+        },
+        events: {
+          onReady: function(e) {
+            e.target.setPlaybackQuality('hd1080');
+            $('#video_02_player').parents().find('.parallaxContent').click(function() {
+              if (e.target.getPlayerState() == 1) {
+                e.target.pauseVideo();
+              }
+              else {
+                e.target.playVideo();
+              }
+            });
           }
         }
       });
