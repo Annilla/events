3de836e51ac8c1deb79e894b2c724bbdaade4471
Anny <anny@juksy.com>
2017-02-20 12:28:11 +0800

    修改 list07, list08 無限次入場動畫

--- js/index.js
+++ js/index.js
@@ -448,6 +448,40 @@
   // plugin wow = scroll to animation
   new WOW().init();
 
+  // plugin showOnScroll for infinite times doing animation
+  $(function() {
+    $(document).on('showOnScroll', function() {
+      var $window = $(window);
+      function showOnScroll() {
+        var scrolled = $window.scrollTop(),
+            wHeight = $window.height();
+        // show
+        $('.showOnScroll:not(.animated)').each(function(){
+          var $this = $(this),
+              offsetTop = $this.offset().top,
+              offset = $this.data('show-offset');
+          if (scrolled + wHeight - offset > offsetTop) {
+            $this.addClass('animated ' + $this.data('show-animation'));
+          }
+        });
+        // hide
+        $(".showOnScroll.animated").each(function() {
+          var $this     = $(this),
+              offsetTop = $this.offset().top,
+              offset = $this.data('show-offset'),
+              oHeight = $(this).height();
+          if (scrolled + wHeight + offset < offsetTop || scrolled > offsetTop + oHeight + offset) {
+            $(this).removeClass('animated');
+            $(this).removeClass($this.data('show-animation'));
+          }
+        });
+      }
+      showOnScroll();
+      $window.scroll(_.throttle(showOnScroll, 250));
+    });
+    $(document).trigger('showOnScroll');
+  });
+
   // plugin img_lazyLoad
   $(function() {
     $(document).on('img_lazyLoad', function() {
@@ -739,7 +773,7 @@
     });
   });
 
-  // Youtube api
+  // Youtube api for header_02
   // create deferred object
   var YTdeferred = $.Deferred();
   window.onYouTubeIframeAPIReady = function() {
